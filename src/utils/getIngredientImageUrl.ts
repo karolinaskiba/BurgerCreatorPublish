3ab3d6ce54e4970283a34fd67af9911540793bd8
karolina.skiba <karolina.skiba@nxsa.onmicrosoft.com>
2024-06-12 19:51:11 +0200

    test

--- src/utils/getIngredientImageUrl.ts
+++ src/utils/getIngredientImageUrl.ts
@@ -5,19 +5,19 @@ export function getImageUrlByIngredientName(
 ): string | null {
   switch (name) {
     case IngredientNameEnum.BOTTOM_BUN:
-      return '/src/assets/bottom-bun.png';
+      return '/bottom-bun.png';
     case IngredientNameEnum.TOP_BUN:
-      return '/src/assets/top-bun.png';
+      return '/top-bun.png';
     case IngredientNameEnum.TOMATO:
-      return '/src/assets/tomato.png';
+      return '/tomato.png';
     case IngredientNameEnum.MEAT:
-      return '/src/assets/ingredient-burger.png';
+      return '/ingredient-burger.png';
     case IngredientNameEnum.FISH:
-      return '/src/assets/ingredient-fish.png';
+      return '/ingredient-fish.png';
     case IngredientNameEnum.LETTUCE:
-      return '/src/assets/lettuce.png';
+      return '/lettuce.png';
     case IngredientNameEnum.CHEESE:
-      return '/src/assets/ingredient-cheese.png';
+      return '/ingredient-cheese.png';
     default:
       return null;
   }
