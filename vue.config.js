8aa15241b654b5cc90fac5fd93ad1ae39ab27b72
karolina.skiba <karolina.skiba@nxsa.onmicrosoft.com>
2024-06-12 18:59:32 +0200

    configuration problems

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,5 @@ module.exports = {
       ? '/BurgerCreatorPublish/' // Zmień na nazwę Twojego repozytorium na GitHub
       : '/',
   outputDir: 'dist/spa', // Katalog, do którego buduje się aplikacja
-  assetsDir: 'static',
+  assetsDir: 'assets',
 };
