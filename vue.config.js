1139122e502da01e234ff794c9c4c368deb80f48
karolina.skiba <karolina.skiba@nxsa.onmicrosoft.com>
2024-06-12 17:34:47 +0200

    -m

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   publicPath:
-    process.env.NODE_ENV === 'production' ? '/BurgerCreatorPublish/' : '/',
-  outputDir: 'dist/spa',
+    process.env.NODE_ENV === 'production'
+      ? '/BurgerCreatorPublish/' // Zmień na nazwę Twojego repozytorium na GitHub
+      : '/',
+  outputDir: 'dist/spa', // Katalog, do którego buduje się aplikacja
   assetsDir: 'static',
 };
