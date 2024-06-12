7cf360072db21aa65ae03ff089f81cbe56678c3a
karolina.skiba <karolina.skiba@nxsa.onmicrosoft.com>
2024-06-12 18:09:52 +0200

    fix image path

--- src/utils/getIngredientImageUrl.ts
+++ src/utils/getIngredientImageUrl.ts
@@ -1,4 +1,3 @@
-// utils/ingredientUtils.ts
 import { IngredientNameEnum } from '../enums/IngredientName.enum';
 
 export function getImageUrlByIngredientName(
@@ -6,19 +5,19 @@ export function getImageUrlByIngredientName(
 ): string | null {
   switch (name) {
     case IngredientNameEnum.BOTTOM_BUN:
-      return '../../src/assets/images/bottom-bun.png';
+      return '/src/assets/images/bottom-bun.png';
     case IngredientNameEnum.TOP_BUN:
-      return '../../src/assets/images/top-bun.png';
+      return '/src/assets/images/top-bun.png';
     case IngredientNameEnum.TOMATO:
-      return '../../src/assets/images/tomato.png';
+      return '/src/assets/images/tomato.png';
     case IngredientNameEnum.MEAT:
-      return '../../src/assets/images/ingredient-burger.png';
+      return '/src/assets/images/ingredient-burger.png';
     case IngredientNameEnum.FISH:
-      return '../../src/assets/images/ingredient-fish.png';
+      return '/src/assets/images/ingredient-fish.png';
     case IngredientNameEnum.LETTUCE:
-      return '../../src/assets/images/lettuce.png';
+      return '/src/assets/images/lettuce.png';
     case IngredientNameEnum.CHEESE:
-      return '../../src/assets/images/ingredient-cheese.png';
+      return '/src/assets/images/ingredient-cheese.png';
     default:
       return null;
   }
